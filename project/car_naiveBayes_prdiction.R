c6fdb224d4682865e932d6de06c5fa2d22c737b5
JEEVA <85932284+Jeeva-AGS@users.noreply.github.com>
2022-08-18 11:38:04 +0530

    Update car_naiveBayes_prdiction.R

--- project/car_naiveBayes_prdiction.R
+++ project/car_naiveBayes_prdiction.R
@@ -2,16 +2,16 @@ install.packages("dplyr")
 install.packages("naivebayes")
 library(naivebayes)
 
-emp_data<-read.csv("C:\\Users\\jeeva\\Study\\SQL\\data-sets\\cars.csv",header=TRUE)
-emp_data
-dim(emp_data)
-str(emp_data)
-summary(emp_data)
-emp_data$Transport=as.factor(emp_data$Transport)
-summary(emp_data)
-ind<-sample(2,nrow(emp_data),replace = TRUE, prob=c(0.8,0.2))
-traindata<-emp_data[ind==1,]
-testdata<-emp_data[ind==2,]
+car_data<-read.csv("C:\\Users\\jeeva\\Study\\SQL\\data-sets\\cars.csv",header=TRUE)
+car_data
+dim(car_data)
+str(car_data)
+summary(car_data)
+car_data$Transport=as.factor(car_data$Transport)
+summary(car_data)
+ind<-sample(2,nrow(car_data),replace = TRUE, prob=c(0.8,0.2))
+traindata<-car_data[ind==1,]
+testdata<-car_data[ind==2,]
 print(traindata)
 print(testdata)
 naive_model<- naive_bayes(Transport ~., data=traindata,type="C-classification")
